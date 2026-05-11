e4a86afdcbf174196fdf15693e72f522f6333f86
Sunpreet Kaur <71538093+sunpreetkr@users.noreply.github.com>
2026-05-11 15:25:16 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_OPBYtAf1k', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7g22unjvmmu435g0p9rop4o2h4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
