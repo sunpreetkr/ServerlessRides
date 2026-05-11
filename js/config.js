cb684a5358ac1ee4402b46130d95594365700bf6
Sunpreet Kaur <71538093+sunpreetkr@users.noreply.github.com>
2026-05-11 15:26:54 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_OPBYtAf1k', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7g22unjvmmu435g0p9rop4o2h4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
